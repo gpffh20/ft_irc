32cda2a6d7692580096796abfd0212725a6c5dea
gykoh42 <karmakoh42@naver.com>
2024-06-26 21:17:14 +0900

    feat: client.hpp 작성

---
+++ inc/client.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <string>
+
+class Client {
+ public:
+  Client(int socket_fd, const std::string& nickname,
+         const std::string& username)
+      : fd(socket_fd), nick(nickname), user(username), is_registered(false) {}
+
+  int getFd() const { return fd; }
+  const std::string& getNick() const { return nick; }
+  const std::string& getUser() const { return user; }
+  bool getIsRegistered() const { return is_registered; }
+  void setRegistered(bool registered) { is_registered = registered; }
+
+ private:
+  int fd;
+  std::string nick;
+  std::string user;
+  bool is_registered;
+};
+
+#endif
