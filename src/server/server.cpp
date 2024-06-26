b63323584f62ed95c034f10a9bd0c1c3c33e2491
gykoh42 <karmakoh42@naver.com>
2024-06-26 21:18:22 +0900

    feat: server.cpp 구현

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,46 +1,134 @@
-#include "../inc/server.hpp"
+#include "../../inc/server.hpp"
 
-Server::Server(const std::string port_num, const std::string password) {
-	this->portnum = setPortNum(port_num);
-	this->passWord = setPassWord(password);
-	setServerSocket();
-	setServerAddr();
-	setServerBind();
-	setServerListen();
-	setServerFd();
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+
+#include <stdexcept>
+
+Server::Server(const std::string& port_num, const std::string& password)
+    : server_fd(-1), client_addr_size(sizeof(client_addr)), fd_count(0) {
+  setPortNum(port_num);
+  setPassWord(password);
+  setServerSocket();
+  setServerAddr();
+  setServerBind();
+  setServerListen();
+  setServerFd();
 }
 
-Server::~Server()
-{
-	if (this->server_fd != -1)
-		close(this->server_fd);
-	if (this->client_fd != -1)
-		close(this->client_fd);
+Server::~Server() {
+  for (std::map<int, Client>::iterator it = clients.begin();
+       it != clients.end(); ++it) {
+    close(it->first);
+  }
+  if (server_fd != -1) close(server_fd);
 }
 
-void Server::run(){
-	while (true) {
-		this->client_fd = accept(this->server_fd, (struct sockaddr *)&this->client_addr, &this->client_addr_size);
-		if (this->client_fd == -1)
-			throw std::runtime_error("accept error");
-		std::cout << "client_fd : " << this->client_fd << std::endl;
-		
-		// fcntl(this->client_fd, F_SETFL, O_NONBLOCK);
-		// this->fds[this->fd_cnt].fd = this->client_fd;
-		// this->fds[this->fd_cnt].events = POLLIN;
-		// this->fd_cnt++;
-		// addClient(this->client_fd);
-		// std::cout << "fd[" << this->client_fd << "] is connected" << std::endl;
-		
-		// if (this->fd_cnt == 256)
-		// 	throw std::runtime_error("fd_cnt is full");
-		
-//		poll 호출
-//		poll 결과 확인
-//		이벤트 발생시 처리
-//		새 클라이언트 연결
-//		기존 클라이언트 메시지 수신
-//		명령어 실행 후 결과 전송
-//		클라이언트 종료
-	}
+void Server::run() {
+  while (true) {
+    int poll_count = poll(fds, fd_count, -1);  // 무한 대기 상태로 폴링
+    if (poll_count == -1) {
+      throw std::runtime_error("Poll failed: " + std::string(strerror(errno)));
+    }
+
+    for (int i = 0; i < fd_count; ++i) {
+      if (fds[i].revents & POLLIN) {
+        if (fds[i].fd == server_fd) {
+          handleNewConnection();
+        } else {
+          handleClientMessages(fds[i].fd);
+        }
+      }
+    }
+  }
+}
+
+void Server::setPortNum(const std::string& port_num) { portnum = port_num; }
+
+void Server::setPassWord(const std::string& password) { passWord = password; }
+
+void Server::setServerSocket() {
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
+    throw std::runtime_error("Could not create socket");
+  }
+  int opt = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+}
+
+void Server::setServerAddr() {
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port =
+      htons(static_cast<unsigned short>(std::atoi(portnum.c_str())));
+}
+
+void Server::setServerBind() {
+  if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    throw std::runtime_error("Bind failed: " + std::string(strerror(errno)));
+  }
+}
+
+void Server::setServerListen() {
+  if (listen(server_fd, 10) == -1) {
+    throw std::runtime_error("Listen failed: " + std::string(strerror(errno)));
+  }
+}
+
+void Server::setServerFd() {
+  fds[0].fd = server_fd;
+  fds[0].events = POLLIN;
+  fd_count = 1;
+}
+
+void Server::addClient(int client_fd) {
+  if (fd_count < MAX_CLIENTS) {
+    fds[fd_count].fd = client_fd;
+    fds[fd_count].events = POLLIN;
+    fd_count++;
+  } else {
+    close(client_fd);  // 최대 클라이언트 수를 초과하면 연결 거부
+  }
+}
+
+void Server::removeClient(int client_fd) {
+  close(client_fd);
+  for (int i = 0; i < fd_count; i++) {
+    if (fds[i].fd == client_fd) {
+      fds[i] = fds[fd_count - 1];
+      fd_count--;
+      break;
+    }
+  }
+}
+
+void Server::handleNewConnection() {
+  int new_fd =
+      accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_size);
+  if (new_fd == -1) {
+    std::cerr << "Error accepting new connection: " << strerror(errno)
+              << std::endl;
+  } else {
+    addClient(new_fd);
+    std::cout << "New client connected: FD " << new_fd << std::endl;
+  }
+}
+
+void Server::handleClientMessages(int client_fd) {
+  char buffer[1024];
+  int nbytes = read(client_fd, buffer, sizeof(buffer));
+  if (nbytes <= 0) {
+    if (nbytes == 0) {
+      std::cout << "Socket " << client_fd << " closed remotely." << std::endl;
+    } else {
+      std::cerr << "Read error: " << strerror(errno) << std::endl;
+    }
+    removeClient(client_fd);
+  } else {
+    std::cout << "Message from " << client_fd << ": "
+              << std::string(buffer, nbytes) << std::endl;
+    // 클라이언트 메시지 처리 로직 추가
+  }
 }
