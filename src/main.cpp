1411a749d96ba7aa8961c54af2a118d9299660aa
gykoh42 <karmakoh42@naver.com>
2024-06-26 21:17:01 +0900

    feat: main.cpp 작성

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-int main(int ac, char *av[])
-{
-	try {
-		if (ac != 3) {
-			std::cout << "Warning :: Argument is only two" << std::endl;
-			return 1;
-		}
-		Server server(av[1], av[2]);
-		server.run();
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
+#include "../inc/server.hpp"
+
+int main(int ac, char *av[]) {
+  try {
+    if (ac != 3) {
+      std::cout << "Warning :: Argument is only two" << std::endl;
+      return 1;
+    }
+    Server server(av[1], av[2]);
+    server.run();
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 }
