5f3f750d8e98f213e4c91ed00510241bc8196b1f
gykoh42 <karmakoh42@naver.com>
2024-07-29 22:27:24 +0900

    feat: ping 명령어 구현

--- inc/command.hpp
+++ inc/command.hpp
@@ -1,30 +1,33 @@
 #ifndef COMMAND_HPP
-# define COMMAND_HPP
+#define COMMAND_HPP
 
+#include <sys/socket.h>
 #include <unistd.h>
-#include <vector>
-#include <string>
+
 #include <iostream>
 #include <map>
+#include <string>
+#include <vector>
 
 class Server;
 
 class Command {
-  public:
-	Command(Server &server);
-	~Command();
-	
-	void run(int client_fd, std::string command, std::vector<std::string> tokens);
-	void quit(int client_fd, std::vector<std::string> tokens);
-	void pass(int client_fd, std::vector<std::string> tokens);
-	void nick(int client_fd, std::vector<std::string> tokens);
-	void ping(int client_fd, std::vector<std::string> tokens);
-	void join(int client_fd, std::vector<std::string> tokens);
-  private:
-	Command &operator=(const Command&);
-	Command(const Command&);
-	
-	 Server		&server_;
+ public:
+  Command(Server &server);
+  ~Command();
+
+  void run(int client_fd, std::string command, std::vector<std::string> tokens);
+  void quit(int client_fd, std::vector<std::string> tokens);
+  void pass(int client_fd, std::vector<std::string> tokens);
+  void nick(int client_fd, std::vector<std::string> tokens);
+  void ping(int client_fd, std::vector<std::string> tokens);
+  void join(int client_fd, std::vector<std::string> tokens);
+
+ private:
+  Command &operator=(const Command &);
+  Command(const Command &);
+
+  Server &server_;
 };
 
 #endif
