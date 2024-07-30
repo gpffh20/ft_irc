2721783909b4f37fba7d3e7994dbe42b84506ce1
gpffh20 <eunbin584836@gmail.com>
2024-07-30 16:40:06 +0900

    fix: sentToClient 추가

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -1,8 +1,11 @@
 #include "../../inc/server.hpp"
 
 void Command::nick(Client& client, std::string token) {
-	token.erase(0, 5);
-	std::cout << "nick token: " << token << std::endl;
-	client.setNickname(token);
-	server_.addNickname(token);
+	std::vector<std::string> args = server_.splitBySpace(token);
+	client.setNickname(args[1]);
+	server_.addNickname(args[1]);
+	server_.sendToClient(client.getFd(), ":" + client.getNickname() + " NICK :" + args[1] + "\r\n");
+
+	std::cout << "nickname: " << client.getNickname() << std::endl;
+	std::cout << "args[1]: " << args[1] << std::endl;
 }
