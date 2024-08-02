42858a6dc5ad8096b50469c2a892047c3f202907
Junsang Mun <mun.js@aol.com>
2024-08-02 14:44:10 +0900

    chroe: join쪽 약간 수정

--- src/cmd/join.cpp
+++ src/cmd/join.cpp
@@ -4,4 +4,7 @@ void Command::join(Client& client, std::vector<std::string> args) {
     (void)client;
     std::cout << args[1] << std::endl;
     Channel newChannel = Channel(args[1]);
+    if (server_.getChannels().find(args[1]) == server_.getChannels().end()) {
+        server_.getChannels().insert(std::pair<std::string, Channel>(args[1], newChannel));
+    }
 }
