f8ed680e5a1168c433e6a689f522768421c05131
Junsang Mun <mun.js@aol.com>
2024-08-06 15:59:50 +0900

    chroe: server.hpp 보기좋게 정리

--- inc/server.hpp
+++ inc/server.hpp
@@ -29,47 +29,48 @@ public:
 
   void run();
   
-  std::map<int, Client>& getClients();
+  std::map<int, Client>&          getClients();
   std::map<std::string, Channel>& getChannels();
-  std::vector<std::string>& getNicknames();
+  std::vector<std::string>&       getNicknames();
+  std::vector<std::string>        splitBySpace(const std::string &str);
   
-  void removeClient(int client_fd);
-  void sendWelcomeMessage(int client_fd);
-  void setPassWord(const std::string &password);
-  std::string& getPassWord();
-  void addNickname(const std::string &nickname);
-  std::vector<std::string> splitBySpace(const std::string &str);
-  void sendToClient(int client_fd, const std::string& message);
-  void removeNickname(const std::string &nickname);
+  void          removeClient(int client_fd);
+  void          sendWelcomeMessage(int client_fd);
+  void          setPassWord(const std::string &password);
+  std::string&  getPassWord();
+  void          addNickname(const std::string &nickname);
+  void          sendToClient(int client_fd, const std::string& message);
+  void          removeNickname(const std::string &nickname);
   
 
 private:
-  void setPortNum(const std::string &port_num);
-  void setServerSocket();
-  void setServerAddr();
-  void setServerBind();
-  void setServerListen();
-  void setServerFd();
-  void addClient(int client_fd);
-  void handleNewConnection();
-  void handleClientMessages(int client_fd);
-  void handleCommands(Client &client);
   std::vector<std::string> splitByCRLF(const std::string &str);
-  void sendToClients();
 
-  int server_fd;
-  struct sockaddr_in server_addr;
-  struct sockaddr_in client_addr;
-  socklen_t client_addr_size;
+  void  setPortNum(const std::string &port_num);
+  void  setServerSocket();
+  void  setServerAddr();
+  void  setServerBind();
+  void  setServerListen();
+  void  setServerFd();
+  void  addClient(int client_fd);
+  void  handleNewConnection();
+  void  handleClientMessages(int client_fd);
+  void  handleCommands(Client &client);
+  void  sendToClients();
 
-  unsigned short portnum;
-  std::string passWord;
+  int                 server_fd;
+  struct sockaddr_in  server_addr;
+  struct sockaddr_in  client_addr;
+  socklen_t           client_addr_size;
 
-  std::map<int, Client> clients;
-  std::map<std::string, Channel> channels;
-  std::vector<std::string> nicknames;
-  struct pollfd fds[MAX_CLIENTS];
-  int fd_count;
+  unsigned short  portnum;
+  std::string     passWord;
+
+  std::map<int, Client>           clients;
+  std::map<std::string, Channel>  channels;
+  std::vector<std::string>        nicknames;
+  struct pollfd                   fds[MAX_CLIENTS];
+  int                             fd_count;
   
   Command	*command_;
 };
