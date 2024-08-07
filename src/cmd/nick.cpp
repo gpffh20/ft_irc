e23334dc16001ce6a32bcf90e906245a1ccea5fb
gpffh20 <eunbin584836@gmail.com>
2024-08-07 23:13:26 +0900

    refactor: 불필요하게 반복되는 코드 수정

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -11,26 +11,23 @@ void Command::nick(Client &client, std::vector<std::string> args) {
 		client.addToSendBuffer(std::string(ERR_NONICKNAMEGIVEN) + " :No nickname given\r\n");
 		return;
 	}
+	std::string nickname = args[1];
 	// 닉네임 중복
-	if (isNicknameExist(args[1], client.getFd())) {
+	if (isNicknameExist(nickname, client.getFd())) {
 		if (client.getNick()) {
 			// 문구 수정하기
-			client.addToSendBuffer(std::string(ERR_NICKNAMEINUSE) + " " + args[1] + " : is already in use\r\n");
+			client.addToSendBuffer(std::string(ERR_NICKNAMEINUSE) + " " + nickname + " :Nickname is already in use\r\n");
+			return;
 		} else {
-			std::string newNickname = args[1] + "_";
-			client.addToSendBuffer(":" + client.getNickname() + " NICK " + newNickname + "\r\n");
-			client.setNickname(newNickname);
-			server_.addNickname(newNickname);
-			client.setNick(true);
+			nickname = nickname + "_";
 		}
-		return ;
 	}
 	if (client.getNick()) {
 		server_.removeNickname(client.getNickname());
 	}
-	client.addToSendBuffer(":" + client.getNickname() + " NICK " + args[1] + "\r\n");
-	client.setNickname(args[1]);
-	server_.addNickname(args[1]);
+	client.addToSendBuffer(":" + client.getNickname() + " NICK " + nickname + "\r\n");
+	client.setNickname(nickname);
+	server_.addNickname(nickname);
 	std::vector<std::string> nicknames = server_.getNicknames();
 	for (std::vector<std::string>::iterator it = nicknames.begin(); it != nicknames.end(); it++) {
 		std::cout << "nickname: " << *it << std::endl;
