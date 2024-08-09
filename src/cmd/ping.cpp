9fe3a3645057157e2010ebfecdfd444db95710c8
gykoh42 <karmakoh42@naver.com>
2024-08-09 22:01:43 +0900

    feat: ping.cpp 수정

--- src/cmd/ping.cpp
+++ src/cmd/ping.cpp
@@ -2,11 +2,10 @@
 
 void Command::ping(Client& client, std::vector<std::string> tokens) {
 		if (tokens.size() == 1) {
-				client.addToSendBuffer(std::string(ERR_NOORIGIN) + " " + client.getNickname() + " :No origin specified\r\n");
+			client.addToSendBuffer(std::string(ERR_NOORIGIN) + " " + client.getNickname() + " :No origin specified\r\n");
 		} else if (tokens.size() == 2) {
-				// 여기서 PONG 출력문이 아니라 PONG 함수를 호출하게 해야 함
-				client.addToSendBuffer("PONG :" + tokens[1] + "\r\n");
+			client.addToSendBuffer("PONG " + tokens[1] + "\r\n");
 		} else {
-				client.addToSendBuffer("PONG :" + tokens[1] + " " + tokens[2] + "\r\n");
+			client.addToSendBuffer("PONG " + tokens[1] + " " + tokens[2] + "\r\n");
 		}
 }
