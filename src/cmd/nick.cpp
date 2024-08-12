b24734e7c7db3df0b29fe28eda5b89d2a80e3cf8
gykoh42 <karmakoh42@naver.com>
2024-08-13 02:35:51 +0900

    feat: 채널에서 nick 변경할 경우 나머지 클라이언트들에게 브로드캐스트 하는 로직 추가

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -11,26 +11,42 @@ void Command::nick(Client &client, std::vector<std::string> args) {
 		client.addToSendBuffer(std::string(ERR_NONICKNAMEGIVEN) + " :No nickname given\r\n");
 		return;
 	}
-	std::string nickname = args[1];
+	std::string newNickname = args[1];
+
 	// 닉네임 중복
-	while (isNicknameExist(nickname, client.getFd())) {
+	while (isNicknameExist(newNickname, client.getFd())) {
 		if (client.getNick()) {
 			client.addToSendBuffer(
-					std::string(ERR_NICKNAMEINUSE) + " " + nickname + " :Nickname is already in use\r\n");
+				std::string(ERR_NICKNAMEINUSE) + " " + newNickname + " :Nickname is already in use\r\n");
 			return;
 		} else {
-			nickname = nickname + "_";
+			newNickname += "_";
 		}
 	}
+
+	// 닉네임 변경 처리
+	std::string oldNickname = client.getNickname();
 	if (client.getNick()) {
-		server_.removeNickname(client.getNickname());
+		server_.removeNickname(oldNickname);
 	}
-	client.addToSendBuffer(":" + client.getNickname() + " NICK " + nickname + "\r\n");
-	client.setNickname(nickname);
-	server_.addNickname(nickname);
-	std::vector<std::string> nicknames = server_.getNicknames();
-	for (std::vector<std::string>::iterator it = nicknames.begin(); it != nicknames.end(); it++) {
-		std::cout << "nickname: " << *it << std::endl;
+	client.setNickname(newNickname);
+	server_.addNickname(newNickname);
+
+	// 특정 채널에서의 닉네임 변경 알림
+	std::map<std::string, Channel>& channels = server_.getChannels();
+	for (std::map<std::string, Channel>::iterator channelIt = channels.begin(); channelIt != channels.end(); ++channelIt) {
+		Channel& channel = channelIt->second;
+		if (channel.isClientInChannel(client)) {
+			std::vector<Client*>& clients = channel.getClientList();
+			for (std::vector<Client*>::iterator it_client = clients.begin(); it_client != clients.end(); ++it_client) {
+				if ((*it_client)->getNickname() != client.getNickname()) { // 자신을 제외하고 알림
+					std::string nickChangeMsg = ":" + oldNickname + "!" + client.getUsername() + "@" + client.getHostname() + " NICK " + newNickname + "\r\n";
+					(*it_client)->addToSendBuffer(nickChangeMsg);
+				}
+			}
+		}
 	}
+
+	client.addToSendBuffer(":" + oldNickname + " NICK " + newNickname + "\r\n");
 	client.setNick(true);
 }
