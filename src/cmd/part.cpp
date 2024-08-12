07c1e8a2fd160a19f87841069c6c0956dbc3f206
gykoh42 <karmakoh42@naver.com>
2024-08-12 18:48:04 +0900

    fix: 서버 종료 버그 수정, 채널의 모든 클라이언트가 나갈 때 채널만 종료되도록 변경

--- src/cmd/part.cpp
+++ src/cmd/part.cpp
@@ -30,13 +30,18 @@ void Command::part(Client &client, std::vector<std::string> args) {
 
 	// 브로드캐스트 메시지 전송 (자신 제외)
 	std::string partMessage = ":" + client.getNickname() + "!" + client.getUsername() + "@" + client.getHostname() + " PART " + channel_name + "\r\n";
-	for (std::vector<Client *>::iterator other_client = clients.begin(); other_client != clients.end(); ++other_client) {
-		if ((*other_client)->getNickname() != client.getNickname()) {
-			(*other_client)->addToSendBuffer(partMessage);
+	for (it_client = clients.begin(); it_client != clients.end(); ++it_client) {
+		if ((*it_client)->getNickname() != client.getNickname()) {
+			(*it_client)->addToSendBuffer(partMessage);
 		}
 	}
 
 	client.addToSendBuffer(partMessage);
 	it->second.removeClient(client);
 	client.removeChannel(&it->second);
+
+	// 채널이 비었는지 검사 후 삭제
+	if (it->second.getClientList().empty()) {
+		server_.getChannels().erase(it);
+	}
 }
