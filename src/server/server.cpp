f879703c014647b1be14e3e53f986abffb85343e
gpffh20 <eunbin584836@gmail.com>
2024-08-15 13:42:01 +0900

    fix: cntl + d 처리 진짜 온료

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -35,7 +35,6 @@ Server::~Server() {
 
 void Server::run() {
 	while (true) {
-		// std::cout << "channel size: " << channels.size() << "\n";
 		// event_count
 		int poll_count = poll(fds, fd_count, -1); // 무한 대기 상태로 폴링
 		if (poll_count == -1) {
@@ -44,7 +43,6 @@ void Server::run() {
 		// event 발생한 만큼 Loop
 		for (int i = 0; i < fd_count; ++i) {
 			if (fds[i].revents & POLLIN) {
-				// std::cout << "POLLIN event on fd: " << fds[i].fd << std::endl;
 				if (fds[i].fd == server_fd) {
 					// 새로운 클라이언트 처리
 					handleNewConnection();
@@ -60,17 +58,6 @@ void Server::run() {
 			}
 		}
 		sendToClients();
-//		bool allClientsEnded = true;
-//		for (std::map<int, Client>::iterator it = clients.begin(); it != clients.end(); ++it) {
-//		    if (!it->second.getEnd()) {
-//		        allClientsEnded = false;
-//		        break;
-//		    }
-//		}
-//
-//		if (allClientsEnded) {
-//    		break;
-//		}
 	}
 }
 
@@ -176,16 +163,6 @@ void Server::removeClient(int client_fd) {
 		}
 	}
 }
-//	close(client_fd);
-//	clients.erase(client_fd);
-//	for (int i = 0; i < fd_count; i++) {
-//		if (fds[i].fd == client_fd) {
-//			fds[i] = fds[fd_count - 1];
-//			fd_count--;
-//			break;
-//		}
-//	}
-//}
 
 void Server::handleNewConnection() {
 	int new_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_size);
@@ -200,16 +177,30 @@ void Server::handleClientMessages(int client_fd) {
 	Client &client = clients.find(client_fd)->second;
 	
 	char buffer[1024];
-	int nbytes = read(client_fd, buffer, sizeof(buffer));
+	int nbytes;
+	static std::string tmp;
+	std::string command;
+	
+	while ((nbytes = read(client_fd, buffer, sizeof(buffer))) > 0) {
+		tmp.append(buffer, nbytes);
+		if (tmp.find("\r\n") != std::string::npos) {
+			break;
+		}
+	}
+	
+	std::cout << "tmp: " << tmp << std::endl;
+	command = tmp;
 	
 	if (nbytes > 0) {
 		// 데이터를 정상적으로 읽었을 때 처리
-		if (std::string(buffer, nbytes).find("CAP LS") != std::string::npos) {
+//		if (std::string(buffer, nbytes).find("CAP LS") != std::string::npos) {
+		if (command.find("CAP LS") != std::string::npos) {
 			removeClient(client_fd);
 			return;
 		}
 		try {
-			client.setMessage(std::string(buffer, nbytes));
+//			client.setMessage(std::string(buffer, nbytes));
+			client.setMessage(command);
 			handleCommands(client);
 		} catch (const std::exception &e) {
 			std::cerr << "Error handling command: " << e.what() << std::endl;
@@ -226,6 +217,7 @@ void Server::handleClientMessages(int client_fd) {
 		std::cerr << "Error reading from client, fd: " << client_fd << std::endl;
 		removeClient(client_fd);
 	}
+	tmp.clear();
 }
 
 std::vector<std::string> Server::splitBySpace(const std::string &str) {
